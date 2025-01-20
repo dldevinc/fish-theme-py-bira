c3dbfb102d1ab6b8a47252433ea1ede4a51bad3d
pix666 <x896321475@gmail.com>
2025-01-20 12:07:59 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<img src="https://cdn.rawgit.com/oh-my-fish/oh-my-fish/e4f1c2e0219a17e2c748b824004c8d0b38055c16/docs/logo.svg" align="left" width="144px" height="144px"/>
+
+#### murik
+> A theme for [Oh My Fish][omf-link] based on [Bira](https://github.com/oh-my-fish/theme-bira).
+
+[![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
+[![Fish Shell Version](https://img.shields.io/badge/fish-v3.0.0-007EC7.svg?style=flat-square)](https://fishshell.com)
+[![Oh My Fish Framework](https://img.shields.io/badge/Oh%20My%20Fish-Framework-007EC7.svg?style=flat-square)](https://www.github.com/oh-my-fish/oh-my-fish)
+
+<br/>
+
+
+## Install
+
+```fish
+$ omf install py-bira
+```
+
+
+# License
+
+[MIT][mit] © [pix666][author]
+
+
+[mit]:            https://opensource.org/licenses/MIT
+[author]:         https://github.com/{{USER}}
+[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+
+[license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
---
+++ fish_prompt.fish
@@ -0,0 +1,63 @@
+function __user_host
+  set -l content 
+  if [ (id -u) = "0" ];
+    echo -n (set_color --bold red)
+  else
+    echo -n (set_color --bold green)
+  end
+  echo -n $USER@(hostname|cut -d . -f 1) (set color normal)
+end
+
+function __current_path
+  echo -n (set_color --bold blue) (pwd) (set_color normal) 
+end
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _git_is_dirty
+  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+end
+
+function __git_status
+  if [ (_git_branch_name) ]
+    set -l git_branch (_git_branch_name)
+
+    if [ (_git_is_dirty) ]
+      set git_info '<'$git_branch"*"'>'
+    else
+      set git_info '<'$git_branch'>'
+    end
+
+    echo -n (set_color yellow) $git_info (set_color normal) 
+  end
+end
+
+function __python_version
+  if type "python3" > /dev/null 2>&1
+    set python_version (python3 -V)
+    echo -n (set_color red) ‹$python_version› (set_color normal)
+  elif type "python" > /dev/null 2>&1
+    set python_version (python -V)
+    echo -n (set_color red) ‹$python_version› (set_color normal)
+  end
+end
+
+function fish_prompt
+  echo -n (set_color white)"╭─"(set_color normal)
+  __user_host
+  __current_path
+  __python_version
+  __git_status
+  echo ''
+  echo (set_color white)"╰─"(set_color --bold white)"\$ "(set_color normal)
+end
+
+function fish_right_prompt
+  set -l st $status
+
+  if [ $st != 0 ];
+    echo (set_color red) ↵ $st(set_color normal)
+  end
+end
